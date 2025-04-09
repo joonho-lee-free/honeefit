e881b0d25c5fae3ace589df7ae10f2301effbe6f
joonho-lee <wefun74@gmail.com>
2025-04-09 23:36:10 +0900

    🎯 START 버튼을 배경 버튼과 동일 위치/크기로 배치

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 export default function Home() {
   const router = useRouter();
 
-  const handleStartQuiz = () => {
+  const handleStart = () => {
     router.push("/quiz");
   };
 
@@ -13,13 +13,17 @@ export default function Home() {
       className="relative w-full h-screen bg-cover bg-center"
       style={{ backgroundImage: 'url("/MAIN.jpg")' }}
     >
-      {/* START 버튼 이미지 */}
-      <div className="absolute bottom-28 left-1/2 transform -translate-x-1/2">
-        <button onClick={handleStartQuiz} className="focus:outline-none">
+      {/* START 버튼 이미지: 위치/크기 정확히 조정 */}
+      <div className="absolute" style={{
+        bottom: "100px",
+        left: "50%",
+        transform: "translateX(-50%)"
+      }}>
+        <button onClick={handleStart} className="focus:outline-none">
           <img
             src="/START.png"
-            alt="MBTI 유형별 신혼여행진단기"
-            className="w-60 sm:w-72 hover:scale-105 transition-transform duration-300"
+            alt="START"
+            className="w-[240px] hover:scale-105 transition-transform duration-300"
           />
         </button>
       </div>
